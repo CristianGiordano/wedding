3adfafb24a5d23ac9928a0af72854bf424c13a61
Cristian Giordano <cristian@sneekdigital.co.uk>
2016-06-30 20:06:37 +0100

    Mobile fixes

--- resources/views/partials/navigation.blade.php
+++ resources/views/partials/navigation.blade.php
@@ -1,17 +1,34 @@
+<style>
+    @media (max-width: 991px) {
+        #nav-menu {
+            height: auto !important;
+        }
+    }
+</style>
 <!-- NAVIGATION -->
 <header id="nav-header">
     <nav id="nav-bar" class="top-bar fluid-width transparent nav-center sticky-nav animation fadeInDown">
 
         <div id="nav-wrapper">
 
+            <div class="logo-wrapper">
+                <a href="/" class="hidden-md hidden-lg">
+                    <div class="css-logo rounded">
+                        <div class="css-logo-text">
+                            <strong>C</strong><i class="de-icon-heart-1"></i><strong>L</strong>
+                        </div>
+                    </div>
+                </a>
+            </div>
+
             <div id="mobile-nav">
                 <i class="de-icon-menu"></i>
             </div>
 
-            <ul id="nav-menu" style="margin-left: -60px;">
+            <ul id="nav-menu">
                 <li class="first-child"><a href="/">HOME</a></li>
                 <li><a href="/on-the-day">ON THE DAY</a></li>
-                <li>
+                <li class="hidden-sm hidden-xs">
                     <a href="/" style="width: 60px; height: 60px; padding: 0; margin: 0 20px;">
                         <div class="css-logo rounded">
                             <div class="css-logo-text">
